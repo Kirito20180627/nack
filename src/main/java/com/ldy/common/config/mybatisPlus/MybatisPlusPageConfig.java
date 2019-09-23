a00d1ec729cb64eb44d27a38bc40d5426ba469fc
lidanyang <2225798292@qq.com>
2019-09-23 12:41:34 +0800

    分页插件

---
+++ src/main/java/com/ldy/common/config/mybatisPlus/MybatisPlusPageConfig.java
@@ -0,0 +1,22 @@
+package com.ldy.common.config.mybatisPlus;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class MybatisPlusPageConfig {
+
+    /**
+     * 分页插件
+     */
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setDialectType("mysql");
+        return page;
+    }
+}
