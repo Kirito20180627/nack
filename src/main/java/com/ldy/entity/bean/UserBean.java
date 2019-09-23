dd095fea3c87b2c073a3fb6680c9922eb7a392ec
lidanyang <2225798292@qq.com>
2019-09-23 12:44:29 +0800

    @Data注释

--- src/main/java/com/ldy/entity/bean/UserBean.java
+++ src/main/java/com/ldy/entity/bean/UserBean.java
@@ -1,7 +1,9 @@
 package com.ldy.entity.bean;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
 
+@Data
 @TableName("user")
 public class UserBean {
 
@@ -10,27 +12,5 @@ public class UserBean {
     private String password;
 
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
